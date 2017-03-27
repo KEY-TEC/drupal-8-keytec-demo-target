1b2d95aaaed7e00ab0fe2e7f062d6cd65003bb5b
abdullahhusseini <abdullah.husseini@key-tec.de>
2017-03-27 11:04:13 +0200

    first export

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -766,3 +766,5 @@ if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
 $settings['simple_environment_indicator'] = 'DarkRed PRD';
+
+$config_directories[CONFIG_SYNC_DIRECTORY] = '../config/sync';
